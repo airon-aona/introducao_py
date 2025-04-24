420695b1f35030bd5cd72876ebad405b371ba3eb
Airon <102803348+airon-aona@users.noreply.github.com>
2025-04-24 10:23:49 -0300

    Update testar_numero_primo.py

--- testar_numero_primo.py
+++ testar_numero_primo.py
@@ -5,11 +5,10 @@ i = 2
 while i < num:
 
     if num%i == 0:
-        print(num,'/',i, ' = ', num/i)
+        print(f'{num}/{i} = {num}/{i}')
         print('Numero não é primo')    
         break
-
+        
     i += 1
-
 else:
-    print(num, 'é um numero primo')
+    print(f'{num} é um numero primo')
