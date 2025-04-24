af094957e5af5a089ac8759574715f5c5ada8b78
Airon <102803348+airon-aona@users.noreply.github.com>
2025-04-24 09:42:17 -0300

    Create divisao.py

---
+++ divisao.py
@@ -0,0 +1,8 @@
+num1 = int(input('Escolha um numero: '));
+num2 = int(input('Escolha outro numero: '))
+
+if num1%num2 == 0:
+    print(num1/num2)
+    print('numeros são divisíveis')
+else :
+    print('Os numeros não são divisíveis')
